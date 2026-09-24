c4eb9be7d464462dfbe5acde27f771fbad758a61
alexanderst2005-lab <alexanderst2005@gmail.com>
2026-09-24 12:03:04 -0500

    Optimizar rendimiento, comprimir imagenes y actualizar logo claro con instagram @bufalostore

--- js/animations.js
+++ js/animations.js
@@ -23,8 +23,8 @@
         }
       });
     }, {
-      threshold: 0.12,
-      rootMargin: '0px 0px -50px 0px'
+      threshold: 0.05,
+      rootMargin: '120px 0px 60px 0px'
     });
 
     revealEls.forEach(el => observer.observe(el));
